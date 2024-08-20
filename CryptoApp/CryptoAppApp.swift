20bdbf1b7b17a73ab3cbbd727b247d5ebb083352
Rohan Prakash <pksh.rohan16@gmail.com>
2024-08-20 22:22:11 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Rohan Prakash on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
