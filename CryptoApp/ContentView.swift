54e818eb11cd736e92c3e90e789a7dde8c184a1c
Rohan Prakash <pksh.rohan16@gmail.com>
2024-08-21 20:32:02 +0530

    project setup

--- CryptoApp/ContentView.swift
+++ CryptoApp/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.theme.accent.ignoresSafeArea()
+            VStack{
+                Text("accentcolor")
+                    .font(.title)
+                    .foregroundStyle(Color.theme.green)
+                    
+                
+            }
+            
         }
-        .padding()
     }
 }
 
---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Rohan Prakash on 20/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+   
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme{
+    
+    let accent = Color("AccentColor")
+    let background = Color ("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secodaryText = Color("SecondaryTextColor")
+}
+
