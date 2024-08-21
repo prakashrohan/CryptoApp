2eb75d1736c3399e55980d7648fb5d41e8a99350
Rohan Prakash <pksh.rohan16@gmail.com>
2024-08-22 00:40:43 +0530

    component design and header view

---
+++ CryptoApp/Extensions/Core/Components/CircleButton/CircleButton.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButton.swift
+//  CryptoApp
+//
+//  Created by Rohan Prakash on 21/08/24.
+//
+
+import SwiftUI
+
+struct CircleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButton()
+}
